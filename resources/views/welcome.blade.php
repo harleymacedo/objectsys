901ce3807756566cd70f01f2bba2c49e96c8a090
Victor Hugo <hugo.victor.n@gmail.com>
2018-09-18 16:51:20 -0300

    Teste 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ObjectSys
+                    ObjectSys agora vai
                 </div>
 
                 <div class="links">
