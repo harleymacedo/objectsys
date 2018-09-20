a99cf0a4817d0fe2b51a6d59550b0a353eb0b6b0
Lucas Eduardo <lucasseduhh@gmail.com>
2018-09-20 13:34:00 -0300

    Teste commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ObjectSys meu teste agora!
+                    ObjectSys meu teste agora!!!
                 </div>
 
                 <div class="links">
