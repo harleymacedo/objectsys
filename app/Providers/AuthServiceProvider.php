1a47e080fb6284659ec8d562fbf75874dd88a442
Victor Hugo <hugo.victor.n@gmail.com>
2018-10-01 16:46:35 -0300

    Correção do AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,14 +25,14 @@ class AuthServiceProvider extends ServiceProvider
      */
 public function boot(Gate $gate)  {
  // Comente essa função antes de fazer o migrate, apos realizar o migrate descomente
-  /*  $this->registerPolicies($gate);
+    /*$this->registerPolicies($gate);
     $permissions = Permission::with('roles')->get();
     foreach($permissions as $permission){
 
     $gate::define($permission->nome,function(User $user)use($permission){
           return $user->hasPermission($permission);
-    });*/
-    }
+    });
+    }*/
         // Comente esta area acima para realizar o comando migrate, apos realizar o migrate descomente
 }
 }
