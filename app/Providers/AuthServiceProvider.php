fcb515af1e77b79ed0b02e1f24f3f693cf4cc2bf
Victor Hugo <hugo.victor.n@gmail.com>
2018-10-10 13:46:00 -0300

    Teste de transferencia de repositorio

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,14 +26,19 @@ class AuthServiceProvider extends ServiceProvider
      */
 public function boot(Gate $gate)  {
  // Comente essa função antes de fazer o migrate, apos realizar o migrate descomente
-    $this->registerPolicies($gate);
-    $permissions = permission::with('roles')->get();
-    foreach($permissions as $permission){
+    try {
+        $this->registerPolicies($gate);
+        $permissions = permission::with('roles')->get();
+        foreach ($permissions as $permission) {
 
-    $gate::define($permission->nome,function(User $user)use($permission){
-          return $user->hasPermission($permission);
-    });
+            $gate::define($permission->nome, function (User $user) use ($permission) {
+                return $user->hasPermission($permission);
+            });
+        }
+    }catch (QueryException $e) {
+        return false;
     }
+
         // Comente esta area acima para realizar o comando migrate, apos realizar o migrate descomente
 }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ObjectSys meu teste agora!!!
+                    Mais um teste!!!
                 </div>
 
                 <div class="links">
@@ -90,3 +90,4 @@
         </div>
     </body>
 </html>
+gi
