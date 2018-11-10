03f4b954caa5d8750db13d22aa7ec7f41e07ab4c
Victor Hugo <hugo.victor.n@gmail.com>
2018-11-09 21:41:29 -0300

    index corrigido

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,7 @@
 <link href="{{ asset('css/index.css') }}" rel="stylesheet">
 
 <div class="bg-color flex-center position-ref full-height">
-<<<<<<< HEAD
-       <div class="content">
-        <a href="{{ route('login') }}"><img class="logo_index" src="{{ asset('images/logo_big.png') }}" alt="logo"></a>
-    </div>
-=======
+
        <div class=" container">
             <div class="row">
                 <div class="col-sm-6">
@@ -18,7 +14,6 @@
                 </div>
             </div>
         </div>
->>>>>>> ffcd86ff3c0c7e5295dfad88523be472d7972587
 </div>
 @endsection
 
