3fdf7670a31cabbfc90fc012e0b368610fe5a0b6
Victor Hugo <hugo.victor.n@gmail.com>
2018-11-17 09:27:55 -0300

    Test https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
+        \URL::forceScheme('https');
     }
 
     /**
