86ee53778b0f1cbf4107ffbccc5c55dcbaf5ba34
Victor Hugo <hugo.victor.n@gmail.com>
2019-02-12 17:19:43 -0300

    minor fixes

--- resources/views/setores/listSetor.blade.php
+++ resources/views/setores/listSetor.blade.php
@@ -4,18 +4,34 @@
 @can('editar')
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 <div class="container">
+    @can('editar')
     <div class="row mt-4">
         <div class="col-sm-12">
             <span class="h1 mb-1 mt-1 float-left">Setores</span>
             <a href="/cadastrar/setor" class="btn btn-outline-secondary mb-1 mt-1 float-right">Cadastrar Setor</a>
         </div>
     </div>
+    @endcan
     <div class="row justify-content-center mb-1 mt-1">
         <div class="col-sm-12">
             <form>
                 <div class="form-row">
-                    <div class="form-group col-sm-11">
+                    <div class="input-group form-group col-sm-8">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text" for=""><i class="fas fa-search-location"></i></label>
+                        </div>
                         <input class="form-control" type="search" placeholder="Buscar...">
+                    </div>
+                    <div class="input-group form-group col-sm-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text" for="filtro"><i class="fas fa-filter"></i></label>
+                        </div>
+                        <select class="custom-select" id="filtro" name="filtro">
+                            <option value="">Filtros</option>
+                            <option value="">Filtros 2</option>
+                            <option value="">Filtros 3</option>
+                            <option value="">Filtros 4</option>
+                        </select>
                     </div>
                     <div class="form-group col-sm-1">
                         <button class="btn btn-dark float-right" type="submit">Buscar</>
