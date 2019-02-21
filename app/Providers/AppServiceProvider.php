d150d114e543926a7a549d6c9074f6552b02228a
juniorbj1993 <juniorbj1993@gmail.com>
2019-02-21 16:34:41 -0300

    layout alterado

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
-        \URL::forceScheme('https');
+        // \URL::forceScheme('https');
     }
 
     /**
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,34 @@
 
 <div class="bg-color flex-center position-ref full-height">
 
-       <div class=" container">
-            <div class="row">
-                <div class="col-sm-6">
-                    <img class="logo_index" src="{{ asset('images/logo_big.png') }}" alt="logo">
 
-                </div>
-                <div class="col-sm-6">
-                    <img class="logo_IFCE" src="{{ asset('images/logo_ifce.png') }}" alt="logo">
-                </div>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="images/slide/slide.jpg" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/slide/slide.jpg" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/slide/slide.jpg" alt="Third slide">
+              </div>
             </div>
-        </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
 </div>
 @endsection
 
