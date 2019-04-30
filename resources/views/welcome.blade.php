a46e7b2b8a3f8b709edde96da86c46fd112b1533
juniorbj1993 <juniorbj1993@gmail.com>
2019-04-30 18:59:56 -0300

    change welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,39 +1,16 @@
-@extends('layouts.app')
-@extends('layouts.sidemenu')
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
 
-@section('contentpage')
-<link href="{{ asset('css/index.css') }}" rel="stylesheet">
-
-<div class="bg-color flex-center position-ref full-height">
-
-
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img class="d-block w-100" src="images/slide/slide.jpg" alt="First slide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="images/slide/slide.jpg" alt="Second slide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="images/slide/slide.jpg" alt="Third slide">
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-
+<div class="container">
+    <div class="row">
+        <div class="md-10 col-12">
+            <img class="img_logo_welcome" src="images/logo_big.png" alt="">
+        </div>
+      
+       <div class="col-12">
+          <a href="{{ route('login') }}" class="entrar_button btn btn-primary btn-lg">Login</a>
+       </div>
+        
+        
+    </div>
 </div>
-@endsection
-
